db31e200bafb71c4a5449647bb358aa039739515
Dustin Milotte <dmilotte@gmail.com>
2019-06-14 20:20:37 -0400

    testing audventure node

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,137 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="rete"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Rete from "rete";
+import ConnectionPlugin from "rete-connection-plugin";
+import VueRenderPlugin from "rete-vue-render-plugin";
+import ContextMenuPlugin from "rete-context-menu-plugin";
+import AreaPlugin from "rete-area-plugin";
+import CommentPlugin from "rete-comment-plugin";
+import HistoryPlugin from "rete-history-plugin";
+// import ConnectionMasteryPlugin from "rete-connection-mastery-plugin";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+var audSocket = new Rete.Socket("AudVenture story");
+
+class InputControl extends Rete.Control {
+  constructor(key) {
+    super(key);
+    // this.render = "js";
+    this.key = key;
+  }
+
+  handler(el, editor) {
+    var input = document.createElement("input");
+    el.appendChild(input);
+
+    var text = this.getData(this.key) || "Some message..";
+
+    input.value = text;
+    this.putData(this.key, text);
+    input.addEventListener("change", () => {
+      this.putData(this.key, input.value);
+    });
   }
 }
+
+class AudComponent extends Rete.Component {
+  constructor() {
+    super("AudNode");
+    this.task = {
+      outputs: { text: "output" }
+    };
+  }
+
+  handler() {
+      // ???
+    var textField = document.createElement("input");
+    this.appendChild(textField);
+    input.value = "enter text here";
+  }
+
+  builder(node) {
+    var inp1 = new Rete.Input("input", "Input", audSocket);
+    var out1 = new Rete.Output("choice1", "Choice", audSocket);
+    var out2 = new Rete.Output("choice2", "Choice", audSocket);
+    // var ctrl = new InputControl("text");
+
+    return (
+      node
+        //   .addControl(ctrl)
+        .addInput(inp1)
+        .addOutput(out1)
+        .addOutput(out2)
+    );
+  }
+
+  worker(node, inputs, outputs) {
+    outputs["num"] = node.id;
+    return { text: node.data.text };
+  }
+}
+
+export default {
+  name: "app",
+  mounted() {
+    (async () => {
+      var container = document.querySelector("#rete");
+      var components = [new AudComponent()];
+
+      var editor = new Rete.NodeEditor("demo@0.1.0", container);
+      editor.use(ConnectionPlugin);
+      editor.use(VueRenderPlugin);
+      editor.use(ContextMenuPlugin);
+      editor.use(AreaPlugin);
+      editor.use(CommentPlugin);
+      editor.use(HistoryPlugin);
+      // editor.use(ConnectionMasteryPlugin);
+
+      var engine = new Rete.Engine("demo@0.1.0");
+
+      components.map(c => {
+        editor.register(c);
+        engine.register(c);
+      });
+
+      var n1 = await components[0].createNode({ num: 2 });
+
+      n1.position = [80, 200];
+
+      editor.addNode(n1);
+
+      editor.on(
+        "process nodecreated noderemoved connectioncreated connectionremoved",
+        async () => {
+          console.log("process");
+          await engine.abort();
+          await engine.process(editor.toJSON());
+        }
+      );
+
+      editor.view.resize();
+      AreaPlugin.zoomAt(editor);
+      editor.trigger("process");
+      console.log(editor.toJSON());
+    })();
+  }
+};
 </script>
 
-<style>
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  width: 100%;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+xus #rete {
+  height: 100vh;
+  width: 100vw;
 }
 </style>
